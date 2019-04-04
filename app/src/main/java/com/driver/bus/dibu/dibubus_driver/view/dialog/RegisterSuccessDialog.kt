d3d53cda763de7505b7f1b04f422323aee7149b9
liaojuan <985434609@qq.com>
2019-04-04 21:11:45 +0800

    上传图片有点问题

--- app/src/main/java/com/driver/bus/dibu/dibubus_driver/view/dialog/RegisterSuccessDialog.kt
+++ app/src/main/java/com/driver/bus/dibu/dibubus_driver/view/dialog/RegisterSuccessDialog.kt
@@ -1,2 +1,22 @@
 package com.driver.bus.dibu.dibubus_driver.view.dialog
 
+import android.content.Context
+import android.view.View
+import com.driver.bus.dibu.dibubus_driver.R
+import com.driver.bus.dibu.dibubus_driver.view.dialog.base.BaseDialog
+
+
+class RegisterSuccessDialog(private val mContext: Context) : BaseDialog(mContext, R.style.dialog), View.OnClickListener{
+
+    init {
+        window.attributes.windowAnimations = R.style.ActionSheetDialogAnimation
+        setContentView(layoutInflater.inflate(R.layout.dialog_register_success, null))
+
+
+    }
+
+    override fun onClick(v: View?) {
+
+    }
+
+}
